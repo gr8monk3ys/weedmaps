087231785c92e0217885a67d343eb72ed669e8cd
gr8monk3ys <lorenzosca7@protonmail.ch>
2025-11-04 13:18:48 -0800

    Update: remove unecessary plots and update sentiment distribution

--- app/plots/sentiment_distribution.py
+++ app/plots/sentiment_distribution.py
@@ -2,10 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Green color scale for cannabis theme
+GREEN_SCALE = "Greens"
+
 
 @st.cache_data()
 def create_sentiment_distribution_plot(data, sentiment_column, title):
     fig = px.histogram(
-        data, x=sentiment_column, title=title, color_continuous_scale=green_scale
+        data, x=sentiment_column, title=title, color_continuous_scale=GREEN_SCALE
+    )
+    fig.update_layout(
+        template="plotly_dark",
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
     )
     return fig
